d0e75cd4caa035ba64fe0b4c4839bd4c23a34080
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-07-20 19:32:43 +0300

    Update Figure2.R

--- Figure2.R
+++ Figure2.R
@@ -8,21 +8,31 @@
 # of multiplicity.
 # ---------------------------------------------------------------------------------------------------------------
 library(ggplot2)
-library(MetBrewer)
+library(RColorBrewer)
 library(ggalt)
 library(gridExtra)
 library(readr)
 
 data_rashomon                  <- read_csv("data_rashomon.csv")
-data_rashomon$resampling_ratio <- paste0("resampling ratio = ", data_rashomon$resampling_ratio)
+data_rashomon$resampling_ratio <- paste0("resampling ratio = ", 
+                                         data_rashomon$resampling_ratio)
 data_rashomon$balancing_method <- factor(data_rashomon$balancing_method,
-                                         levels = c("Original", "Undersampling", "Near miss", 
-                                                    "Oversampling", "SMOTE"))
+                                         levels = c("Original", 
+                                                    "Undersampling", 
+                                                    "Near miss", 
+                                                    "Oversampling", 
+                                                    "SMOTE"))
 
-ggplot(data_rashomon, aes(x = obscurity, y = discrepancy, col = balancing_method)) +
-  geom_point(size = 1, alpha = 0.7) +
+ggplot(data_rashomon, aes(x   = obscurity, 
+                          y   = discrepancy, 
+                          col = balancing_method)) +
+  geom_point(size  = 1, 
+             alpha = 0.8) +
   xlim(c(0, 0.4)) + 
-  labs(x = "obscurity", y = "discrepancy", col = "", fill = "") +
+  labs(x    = "obscurity", 
+       y    = "discrepancy", 
+       col  = "", 
+       fill = "") +
   theme_bw() + 
   theme(legend.position = "top",
         axis.text.x  = element_text(size = 18),    
@@ -31,8 +41,18 @@ ggplot(data_rashomon, aes(x = obscurity, y = discrepancy, col = balancing_method
         axis.title.y = element_text(size = 18),
         strip.text.x = element_text(size = 18),
         legend.text  = element_text(size = 18)) + 
-  geom_density_2d(aes(group = balancing_method, col = balancing_method), alpha = 0.5, linewidth = 0.2) + 
-  geom_encircle(aes(fill = balancing_method), s_shape = 2, expand = 0, 
-                spread = 0.02, alpha = 0.2, show.legend = TRUE) + 
-  facet_wrap(~ resampling_ratio, ncol = 1)
+  geom_density_2d(aes(group  = balancing_method, 
+                      col    = balancing_method), 
+                  alpha      = 0.5, 
+                  linewidth  = 0.2) + 
+  geom_encircle(aes(fill     = balancing_method), 
+                s_shape      = 2, 
+                expand       = 0, 
+                spread       = 0.02, 
+                alpha        = 0.2, 
+                show.legend  = TRUE) + 
+  scale_color_brewer(palette = "Set1") +
+  scale_fill_brewer(palette  = "Set1") +
+  facet_wrap(~ resampling_ratio, 
+             ncol = 1)
 # ---------------------------------------------------------------------------------------------------------------
